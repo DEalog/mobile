51d5e692b81843a1c682377557a64fda5f89941c
Andreas Gross <andreas@gross-server.de>
2020-08-06 00:18:07 +0200

    Fixed unittest issue

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,7 +4,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Home'),
+      child: Text('Home', textDirection: TextDirection.ltr),
     );
   }
 }
