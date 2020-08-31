f210e1277801446464cf3394562dfa75fc392f92
Andreas Würl <andreas.wuerl@allianz.de>
2020-09-01 01:31:11 +0200

    DEV-23 Channel configuration on settings page

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/settings.dart';
+import 'package:mobile/settings/channel.dart';
+
+import '../main.dart';
 
 class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final appSettings = getIt<AppSettings>();
     return Container(
-      child: Text('Settings'),
-    );
+        child: ListView(children: [ChannelSettings(appSettings.locations)]));
   }
 }
