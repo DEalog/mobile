3d9ae1f13956179fcec587e994fb6a5ae2252392
Andreas Gross <andreas@gross-server.de>
2021-03-20 22:16:19 +0100

    Pull to Refresh added

--- lib/ui_kit/message_card_ui.dart
+++ lib/ui_kit/message_card_ui.dart
@@ -3,25 +3,28 @@ import 'package:flutter/material.dart';
 class MessageCardUi extends StatelessWidget {
   final String identifier;
   final String description;
+  final double width;
 
-  const MessageCardUi({Key key, this.identifier, this.description})
-      : super(key: key);
+  const MessageCardUi({
+    Key key,
+    this.identifier,
+    this.description,
+    this.width = -1.0,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var contextSize = MediaQuery.of(context).size;
-    
-    return Container(
-      width: contextSize.width * 0.8,
-      // height: MediaQuery.of(context).size.width * 0.1,
+    var _width = this.width < 0 ? contextSize.width * 0.8 : this.width;
+
+    return SizedBox(
+      width: _width,
       child: Card(
         clipBehavior: Clip.antiAlias,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15.0),
         ),
         child: ListTile(
-          // contentPadding: EdgeInsets.fromLTRB(4.0, 4.0, 4.0, 20.0),
-
           leading: Icon(
             Icons.warning_amber_rounded,
             size: contextSize.height * 0.06,
@@ -31,34 +34,11 @@ class MessageCardUi extends StatelessWidget {
             padding: EdgeInsets.only(bottom: contextSize.height * 0.02),
             child: Text(this.identifier),
           ),
-          subtitle:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Wrap(children: [
-              Text(
-                this.description,
-                maxLines: 6,
-                overflow: TextOverflow.ellipsis,
-              ),
-            ])
-          ]),
-
-          // Container(
-          //     width: MediaQuery.of(context).size.width * 0.8,
-          //     child: Row(
-          //       children: <Widget>[
-          //         Expanded(
-          //           child: Text(this.description),
-          //         )
-          //       ],
-          //     ))
-
-          // Text(
-          //   this.description.length > 150
-          //       ? this
-          //           .description
-          //           .replaceRange(140, this.description.length, '...')
-          //       : this.description,
-          // ),
+          subtitle: Text(
+            this.description,
+            maxLines: 4,
+            overflow: TextOverflow.ellipsis,
+          ),
         ),
       ),
     );
